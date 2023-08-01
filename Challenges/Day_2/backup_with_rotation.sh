2784c038d641a8b81cbef94d3c75857bcb807d56
Harsh Soni <soniharsh650@gmail.com>
2023-08-01 16:06:54 +0530

    update day_2.1

--- Challenges/Day_2/backup_with_rotation.sh
+++ Challenges/Day_2/backup_with_rotation.sh
@@ -1,38 +1,56 @@
 #!/bin/bash
 
-# Check if the directory path is provided as a command-line argument
-if [ $# -eq 0 ]; then
-    echo "Error: Please provide the directory path as a command-line argument."
-    exit 1
-fi
+# # Check if the directory path is provided as a command-line argument
+# if [ $# -eq 0 ]; then
+#     echo "Error: Please provide the directory path as a command-line argument."
+#     exit 1
+# fi
 
-backup_directory=$1
+# backup_directory=$1
 
-# Create a timestamped backup folder
-timestamp=$(date +%Y%m%d%H%M%S)
-backup_folder="${backup_directory}/backup_${timestamp}"
-mkdir -p "$backup_folder"
+# # Create a timestamped backup folder
+# timestamp=$(date +%Y%m%d%H%M%S)
+# backup_folder="${backup_directory}/backup_${timestamp}"
+# mkdir -p "$backup_folder"
 
-# Copy all files from the specified directory into the backup folder
-cp -r "${backup_directory}"/* "$backup_folder"
+# # Copy all files from the specified directory into the backup folder
+# cp -r "${backup_directory}"/* "$backup_folder"
 
-# Use rsync to copy all files from the specified directory into the backup folder
-#rsync -av --exclude="${backup_folder}" "${backup_directory}/" "$backup_folder"
+# # Use rsync to copy all files from the specified directory into the backup folder
+# #rsync -av --exclude="${backup_folder}" "${backup_directory}/" "$backup_folder"
 
-# Implement rotation to keep only the last 3 backups
-backup_folders=(${backup_directory}/backup_*)
+# # Implement rotation to keep only the last 3 backups
+# backup_folders=(${backup_directory}/backup_*)
 
-if [ "${#backup_folders[@]}" -gt 3 ]; then
-    # Sort backup folders by timestamp in ascending order
-    sorted_backup_folders=($(printf "%s\n" "${backup_folders[@]}" | sort -n))
+# if [ "${#backup_folders[@]}" -gt 3 ]; then
+#     # Sort backup folders by timestamp in ascending order
+#     sorted_backup_folders=($(printf "%s\n" "${backup_folders[@]}" | sort -n))
 
-    # Determine the number of folders to remove
-    folders_to_remove=$(( ${#sorted_backup_folders[@]} - 3 ))
+#     # Determine the number of folders to remove
+#     folders_to_remove=$(( ${#sorted_backup_folders[@]} - 3 ))
 
-    # Remove the oldest backup folders to keep only the last 3
-    for (( i=0; i<"$folders_to_remove"; i++ )); do
-        rm -rf "${sorted_backup_folders[$i]}"
-    done
-fi
+#     # Remove the oldest backup folders to keep only the last 3
+#     for (( i=0; i<"$folders_to_remove"; i++ )); do
+#         rm -rf "${sorted_backup_folders[$i]}"
+#     done
+# fi
 
-echo "Backup completed successfully."
+# echo "Backup completed successfully."
+
+#we will take source path from user and gets stored in $1
+src_path=$1
+
+#we will store the current date and time in 2 variables
+fname="$(date '+%Y-%m-%d')"
+time="$(date '+%I-%M-%S')"
+
+#storing the name of the backup file in f_name
+f_name=backup_$fname"_"$time.tar.gz
+
+#this line will zip the files present in $src_path
+tar -czf $f_name $src_path
+
+echo "Backup Created: $PWD/$f_name"
+
+#deleting the files except the 3 latest backup files
+rm -f $(ls -1t *.tar.gz | tail -n +4)
--- Challenges/Day_2/explorer.sh
+++ Challenges/Day_2/explorer.sh
@@ -1,37 +1,57 @@
 #!/bin/bash
 
+# echo "Welcome to the Interactive File and Directory Explorer!"
+# echo -e "\033[32m===========================================\033[0m"
+
+# ### Part 1: File and Directory Exploration
+# echo "Files and Directories in the Current Path:"
+
+# # Function to get human-readable size
+# get_human_readable_size() {
+#     du -h "$1" | awk '{print $1}'
+# }
+# # List files and directories in the desired order
+# find . -maxdepth 1 -type f -print0 | while IFS= read -r -d '' file; do
+#     size=$(get_human_readable_size "$file")
+#     printf "%s (%s)\n" "$(basename "$file")" "$size"
+# done
+
+# find . -maxdepth 1 -type d -print0 | while IFS= read -r -d '' dir; do
+#     size=$(get_human_readable_size "$dir")
+#     printf "%s (%s)\n" "$(basename "$dir")" "$size"
+# done
+
+# echo -e "\033[32m===========================================\033[0m"
+
+
+# while true; do
+#     read -p "Enter a line of text (Press Enter without text to exit): " input
+
+#     if [[ -z "$input" ]]; then
+#         echo "Exiting the Interactive Explorer. Goodbye!"
+#         break
+#     fi
+
+#     char_count=${#input}
+#     echo "Character Count: $char_count"
+# done
+
 echo "Welcome to the Interactive File and Directory Explorer!"
-echo -e "\033[32m===========================================\033[0m"
 
-### Part 1: File and Directory Exploration
+#ls -lh will give us the list of files and directories with their size in human readable format
+# awk will help us to print the 9th and 5th column of that output
 echo "Files and Directories in the Current Path:"
+ls -lh | awk '{print "-",$10,"(",$6,")"}'
 
-# Function to get human-readable size
-get_human_readable_size() {
-    du -h "$1" | awk '{print $1}'
-}
-# List files and directories in the desired order
-find . -maxdepth 1 -type f -print0 | while IFS= read -r -d '' file; do
-    size=$(get_human_readable_size "$file")
-    printf "%s (%s)\n" "$(basename "$file")" "$size"
+#checking if the length of msg variable is 0, it will break out of the loop
+while [ True ];
+do
+	read -p "Enter a line of text(Press enter without text to exit): " msg
+	if [ ${#msg} -eq 0 ]
+	then
+		break	
+	fi	
+	echo "Character Count: ${#msg} "
 done
 
-find . -maxdepth 1 -type d -print0 | while IFS= read -r -d '' dir; do
-    size=$(get_human_readable_size "$dir")
-    printf "%s (%s)\n" "$(basename "$dir")" "$size"
-done
-
-echo -e "\033[32m===========================================\033[0m"
-
-
-while true; do
-    read -p "Enter a line of text (Press Enter without text to exit): " input
-
-    if [[ -z "$input" ]]; then
-        echo "Exiting the Interactive Explorer. Goodbye!"
-        break
-    fi
-
-    char_count=${#input}
-    echo "Character Count: $char_count"
-done
+echo "Exiting the Interactive Explorer. Goodbye!"
